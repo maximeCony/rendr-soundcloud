ba14dc87b9828d7a2649aade12ffb7bd4d9c4f76
maximeCony <maxime.cony@gmail.com>
2015-04-28 20:12:36 -0700

    add limit to likes

--- app/controllers/like_controller.js
+++ app/controllers/like_controller.js
@@ -5,6 +5,7 @@ module.exports = {
   index: function(params, callback) {
     // TODO: find a better way
     params.client_id = 'ff4519ae1db0890f2ffb602c3bc0c8bd';
+    if (!params.limit) params.limit = 30;
     var spec = {
       collection: { collection: 'Likes', params: params }
     };
